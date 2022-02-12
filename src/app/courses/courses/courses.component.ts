6416c425cea95df3d589075210ae93920e094ec3
Gabriela R <ladyg432@gmail.com>
2022-02-12 14:07:15 -0300

    Adicionando um service

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { CoursesService } from './../services/courses.service';
 import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
 
@@ -13,8 +14,13 @@ export class CoursesComponent implements OnInit {
   ];
   displayedColumns = ['name', 'category'];
 
-  constructor() {
+  //coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
    // this.courses =[];
+   //this.coursesService = new CoursesService();
+   this.courses= this.coursesService.list();
+
   }
 
   ngOnInit(): void {
